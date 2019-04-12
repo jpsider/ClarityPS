2810b5e769b1c31655a836404768ba292fd7a96d
jpsider <jpsider@gmail.com>
2019-04-12 19:49:24 -0400

    checking in template files.

---
+++ ClarityPS/private/New-FlexContainer.ps1
@@ -0,0 +1,42 @@
+function New-ReportCard
+{
+    <#
+    .DESCRIPTION
+        Builds HTML Reports using VMware's ClarityUI library.
+    .PARAMETER tbd01
+        working on the details
+    .PARAMETER tbd02
+        working on the details
+    .EXAMPLE
+        New-ReportCard
+    .NOTES
+        No notes at this time.
+    #>
+    [CmdletBinding(
+        SupportsShouldProcess = $true,
+        ConfirmImpact = "Low"
+    )]
+    [OutputType([String])]
+    param(
+        [Parameter()][String]$tbd01,
+        [Parameter()][String]$tbd02
+    )
+    if ($pscmdlet.ShouldProcess("Starting New-ReportCard function."))
+    {
+        try
+        {
+            #Add Function details
+        }
+        catch
+        {
+            $ErrorMessage = $_.Exception.Message
+            $FailedItem = $_.Exception.ItemName
+            Throw "New-ReportCard: $ErrorMessage $FailedItem"
+        }
+    }
+    else
+    {
+        # -WhatIf was used.
+        return $false
+    }
+}
---
+++ tests/Private/Close-FlexItem.Tests.ps1
@@ -0,0 +1,15 @@
+$script:ModuleName = 'ReportCardPS'
+
+$here = (Split-Path -Parent $MyInvocation.MyCommand.Path) -replace 'tests', "$script:ModuleName"
+$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
+. "$here\$sut"
+
+Describe "New-ReportCard function for $script:ModuleName" -Tags Build
+{
+    It "Should return False if -WhatIf is used." {
+        New-ReportCard -WhatIf | Should be $false
+}
+It "Should Return true." {
+    New-ReportCard | Should be $true
+}
+}
