e66507036c6f9db72e19bbf48622ba3a665c4dd0
jpsider <jpsider@gmail.com>
2019-05-01 16:13:06 -0400

    updated tests

--- ClarityPS/public/New-HtmlDocument.ps1
+++ ClarityPS/public/New-HtmlDocument.ps1
@@ -1,14 +1,12 @@
-function New-ReportCard
+function New-HtmlDocument
 {
     <#
     .DESCRIPTION
-        Builds HTML Reports using VMware's ClarityUI library.
-    .PARAMETER tbd01
-        working on the details
-    .PARAMETER tbd02
-        working on the details
+        Adds HTML Element
+    .PARAMETER Title
+        Title, used for comment
     .EXAMPLE
-        New-ReportCard
+        New-HtmlDocument -Title MyHTMLDocument
     .NOTES
         No notes at this time.
     #>
@@ -17,23 +15,22 @@ function New-ReportCard
         ConfirmImpact = "Low"
     )]
     [OutputType([String])]
-    [OutputType([boolean])]
+    [OutputType([Boolean])]
     param(
-        [Parameter()][String]$tbd01,
-        [Parameter()][String]$tbd02
+        [Parameter()][String]$Title
     )
-    if ($pscmdlet.ShouldProcess("Starting New-ReportCard function."))
+    if ($pscmdlet.ShouldProcess("Starting New-HtmlDocument function."))
     {
-        try
+        # Determine if a title was specified
+        if ($Title)
         {
-            #Add Function details
+            $HtmlString = "<!DOCTYPE html><html lang='en'><! Start $Title>"
         }
-        catch
+        else
         {
-            $ErrorMessage = $_.Exception.Message
-            $FailedItem = $_.Exception.ItemName
-            Throw "New-ReportCard: $ErrorMessage $FailedItem"
+            $HtmlString = "<!DOCTYPE html><html lang='en'>"
         }
+        $HtmlString
     }
     else
     {
